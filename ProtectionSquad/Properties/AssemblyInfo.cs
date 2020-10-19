c101ca7962cfe564601a7f9fefa26476e7879444
LtSterben <73133164+LtSterben@users.noreply.github.com>
2020-10-19 19:04:19 +0100

    Add files via upload

---
+++ ProtectionSquad/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyTitle("LSPS - Los Santos Protection Squad")]
+[assembly: AssemblyDescription("GTA 5 / LSPDFR Plugin.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Connor.S")]
+[assembly: AssemblyProduct("ProtectionSquad")]
+[assembly: AssemblyCopyright("Copyright © Connor.S")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8fb4db44-9f91-43d6-a84f-4af32e6ba9be")]
+[assembly: AssemblyFileVersion("2.0")]
