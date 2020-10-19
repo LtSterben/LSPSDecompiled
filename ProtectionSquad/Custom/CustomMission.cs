39d1b88fa9392220a58413a0861302f8ec919697
LtSterben <73133164+LtSterben@users.noreply.github.com>
2020-10-19 19:01:33 +0100

    Create CustomMission.cs

---
+++ ProtectionSquad/Custom/CustomMission.cs
@@ -0,0 +1,137 @@
+using System;
+
+namespace Protection_Squad.Custom
+{
+	// Token: 0x02000019 RID: 25
+	public class CustomMission
+	{
+		// Token: 0x040000EA RID: 234
+		public string name = "custom";
+
+		// Token: 0x040000EB RID: 235
+		public string startName = "custom";
+
+		// Token: 0x040000EC RID: 236
+		public string destinationName = "custom";
+
+		// Token: 0x040000ED RID: 237
+		public float startposprotaX;
+
+		// Token: 0x040000EE RID: 238
+		public float startposprotaY;
+
+		// Token: 0x040000EF RID: 239
+		public float startposprotaZ;
+
+		// Token: 0x040000F0 RID: 240
+		public float startposlimoX;
+
+		// Token: 0x040000F1 RID: 241
+		public float startposlimoY;
+
+		// Token: 0x040000F2 RID: 242
+		public float startposlimoZ;
+
+		// Token: 0x040000F3 RID: 243
+		public float startposfirstvX;
+
+		// Token: 0x040000F4 RID: 244
+		public float startposfirstvY;
+
+		// Token: 0x040000F5 RID: 245
+		public float startposfirstvZ;
+
+		// Token: 0x040000F6 RID: 246
+		public float startpossecondvX;
+
+		// Token: 0x040000F7 RID: 247
+		public float startpossecondvY;
+
+		// Token: 0x040000F8 RID: 248
+		public float startpossecondvZ;
+
+		// Token: 0x040000F9 RID: 249
+		public float heStartProta = 1f;
+
+		// Token: 0x040000FA RID: 250
+		public float heStartLimo = 1f;
+
+		// Token: 0x040000FB RID: 251
+		public float heStartFirst = 1f;
+
+		// Token: 0x040000FC RID: 252
+		public float heStartSecond = 1f;
+
+		// Token: 0x040000FD RID: 253
+		public float endposprotaX;
+
+		// Token: 0x040000FE RID: 254
+		public float endposprotaY;
+
+		// Token: 0x040000FF RID: 255
+		public float endposprotaZ;
+
+		// Token: 0x04000100 RID: 256
+		public float endposlimoX;
+
+		// Token: 0x04000101 RID: 257
+		public float endposlimoY;
+
+		// Token: 0x04000102 RID: 258
+		public float endposlimoZ;
+
+		// Token: 0x04000103 RID: 259
+		public float endposfirstvX;
+
+		// Token: 0x04000104 RID: 260
+		public float endposfirstvY;
+
+		// Token: 0x04000105 RID: 261
+		public float endposfirstvZ;
+
+		// Token: 0x04000106 RID: 262
+		public float endpossecondvX;
+
+		// Token: 0x04000107 RID: 263
+		public float endpossecondvY;
+
+		// Token: 0x04000108 RID: 264
+		public float endpossecondvZ;
+
+		// Token: 0x04000109 RID: 265
+		public float heEndProta = 1f;
+
+		// Token: 0x0400010A RID: 266
+		public float heEndLimo = 1f;
+
+		// Token: 0x0400010B RID: 267
+		public float heEndFirst = 1f;
+
+		// Token: 0x0400010C RID: 268
+		public float heEndSecond = 1f;
+
+		// Token: 0x0400010D RID: 269
+		public float startposVipX;
+
+		// Token: 0x0400010E RID: 270
+		public float startposVipY;
+
+		// Token: 0x0400010F RID: 271
+		public float startposVipZ;
+
+		// Token: 0x04000110 RID: 272
+		public float heStartVip;
+
+		// Token: 0x04000111 RID: 273
+		public float endposVipX;
+
+		// Token: 0x04000112 RID: 274
+		public float endposVipY;
+
+		// Token: 0x04000113 RID: 275
+		public float endposVipZ;
+
+		// Token: 0x04000114 RID: 276
+		public float heEndVip;
+	}
+}
