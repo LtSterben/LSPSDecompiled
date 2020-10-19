b691c6d8e52741cc34ddabc073d9e78c8dcfe95c
LtSterben <73133164+LtSterben@users.noreply.github.com>
2020-10-19 19:01:46 +0100

    Add files via upload

---
+++ ProtectionSquad/Custom/CustomVehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Protection_Squad.Custom
+{
+	// Token: 0x0200001A RID: 26
+	public class CustomVehicle
+	{
+		// Token: 0x04000115 RID: 277
+		public string name = "xml";
+
+		// Token: 0x04000116 RID: 278
+		public string model = "ADDER";
+
+		// Token: 0x04000117 RID: 279
+		public string type = "Limo";
+	}
+}
